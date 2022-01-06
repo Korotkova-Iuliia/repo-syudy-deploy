5646262d77bdc8bdc8e45d9e35fcd25b07a16591
Korotkova-Iuliia <3589900@gmail.com>
2022-01-06 15:42:15 +0200

    button

--- src/index.js
+++ src/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App>
+//       <div>hello</div>
+//     </App>
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App>
-      <div>hello</div>
-    </App>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+class Counter extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+  handleIncrement = evt => {
+    console.log('Increment button was clicked!', evt); // работает
+    console.log('this.props: ', this.props); // работает
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleDecrement = evt => {
+    console.log('Decrement button was clicked!', evt); // работает
+    console.log('this.props: ', this.props); // работает
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>0</span>
+        <button type="button" onClick={this.handleIncrement}>
+          Increment by {step}
+        </button>
+        <button type="button" onClick={this.handleDecrement}>
+          Decrement by {step}
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter step={7} />, document.getElementById('root'));
